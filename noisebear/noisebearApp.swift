d0f06b5d1a3512de2d22f25559eb88b653c54aee
christophhu <christoph.huschenhoefer@gmx.de>
2025-08-07 12:18:03 +0200

    Initial Commit

---
+++ noisebear/noisebearApp.swift
@@ -0,0 +1,17 @@
+//
+//  noisebearApp.swift
+//  noisebear
+//
+//  Created by Christoph Huschenhöfer on 07.08.25.
+//
+
+import SwiftUI
+
+@main
+struct noisebearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
